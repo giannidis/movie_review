9f9637ea64e16995c17ad6b91e675986006cabfc
Giannis Dem <giannidis50@hotmail.com>
2015-02-28 18:58:01 +0200

    Review Scaffold

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+	before_action :set_review, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
+
+	def new
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+  end
+
+  def update
+    @review.update(review_params)
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to root_path
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
