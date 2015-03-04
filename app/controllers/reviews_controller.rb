c3d1fff7cede5d0ac6829aba7ae7e1513bd4ae81
Giannis Dem <giannidis50@hotmail.com>
2015-03-05 00:16:07 +0200

    Convert review ratings to stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,38 +1,46 @@
 class ReviewsController < ApplicationController
 	before_action :set_review, only: [:show, :edit, :update, :destroy]
+	before_action :set_movie
 	before_action :authenticate_user!
 
 	def new
-    @review = Review.new
-  end
+		@review = Review.new
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def create
-    @review = Review.new(review_params)
-    @review.user_id = current_user.id
-  end
+	def create
+		@review = Review.new(review_params)
+		@review.user_id = current_user.id
+		@review.movie_id = @movie.id
 
-  def update
-    @review.update(review_params)
-  end
+		if @review.save
+			redirect_to @movie
+		else
+			render 'new'
+		end
+	end
 
-  def destroy
-    @review.destroy
-    redirect_to root_path
-  end
+	def update
+		@review.update(review_params)
+	end
 
-  private
-    def set_review
-      @review = Review.find(params[:id])
-    end
+	def destroy
+		@review.destroy
+		redirect_to root_path
+	end
 
-    def set_movie
-      @movie = Movie.find(params[:movie_id])
-    end
+	private
+		def set_review
+			@review = Review.find(params[:id])
+		end
 
-    def review_params
-      params.require(:review).permit(:rating, :comment)
-    end
+		def set_movie
+			@movie = Movie.find(params[:movie_id])
+		end
+
+		def review_params
+			params.require(:review).permit(:rating, :comment)
+		end
 end
